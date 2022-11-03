df9b8b323a1f46df8c6c8ccd13313c122de8b109
Eduardy Lopes <eduardy.morais@ostenmoove.com.br>
2022-11-03 20:13:14 -0300

    create products table

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,13 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamoDb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
 
 import { Construct } from 'constructs';
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+  readonly productsDdb: dynamoDb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.productsDdb = new dynamoDb.Table(this, 'ProductsDdb', {
+      tableName: 'products',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'id',
+        type: dynamoDb.AttributeType.STRING,
+      },
+      billingMode: dynamoDb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
       this,
@@ -22,7 +36,12 @@ export class ProductsAppStack extends cdk.Stack {
           minify: true,
           sourceMap: false,
         },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
       }
     );
+
+    this.productsDdb.grantReadData(this.productsFetchHandler);
   }
 }
